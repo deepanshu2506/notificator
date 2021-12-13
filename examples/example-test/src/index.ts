2bde267d534fe070b00592c9747d3dbe93d981b8
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-12-13 19:57:02 +0530

    update example

--- examples/example-test/src/index.ts
+++ examples/example-test/src/index.ts
@@ -1,22 +1,38 @@
-import { ChannelTypes, Notificator } from "@notificator/core";
+import {
+  ChannelTypes,
+  ISendMessageFailureResponse,
+  Notificator,
+  ResponseStates,
+} from "@notificator/core";
 import { ConsoleTransport } from "@notificator/console-transport";
+import { SendGridTransport } from "@notificator/sendgrid";
+import { IEmailTemplate } from "packages/core/lib/template/template.types";
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log(process.env);
+
 const notifier = new Notificator();
 
 const consoleTransport = new ConsoleTransport({ id: "abc" });
+const sendgridTransport = new SendGridTransport({
+  apiKey: process.env.SENDGRID_API_KEY || "",
+  from: "deepanshu.v@somaiya.edu",
+  id: "sendgrid-test",
+});
 notifier.registerTransport(consoleTransport);
+notifier.registerTransport(sendgridTransport);
 
 const template: string = `hello {{person.firstname}} {{person.lastname}}`;
+const consoleTemplate: IEmailTemplate = {
+  channel: ChannelTypes.EMAIL,
+  subject: "hello",
+  name: "hello-template",
+  template,
+};
 notifier.registerTemplates({
   event: "abc",
-  templates: [
-    {
-      channel: ChannelTypes.EMAIL,
-      subject: "hello",
-      name: "hello-template",
-      template,
-    },
-    {},
-  ],
+  templates: [consoleTemplate],
 });
 
 notifier
@@ -27,4 +43,9 @@ notifier
   })
   .then((res) => {
     console.log(res);
+    res.map((item) => {
+      if (item.status === ResponseStates.FAILURE) {
+        console.log((item as ISendMessageFailureResponse).reason);
+      }
+    });
   });
